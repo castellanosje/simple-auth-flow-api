5c7979a37777b063ef1eda39a8106214d73c3d1d
Jaime Castellanos <je34429@gmail.com>
2024-08-16 11:46:35 -0600

    removes distro

--- backend/dist/authConfig.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SECRET = void 0;
-exports.SECRET = "jelkwqreiqwoejwqp31e3sie2123j124ir9w0";
--- backend/dist/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const user_controllers_1 = require("../controllers/user.controllers");
-const router = (0, express_1.Router)();
-router.post("/auth/register", user_controllers_1.registerUser);
-router.post('/auth/login', user_controllers_1.loginUser);
-exports.default = router;
